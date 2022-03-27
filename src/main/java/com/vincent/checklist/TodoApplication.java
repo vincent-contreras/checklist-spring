29bb76c7cc09188142209f9537fd64bc83ab3ede
Paul Vincent Contreras <vincent.contreras@outlook.com>
2022-03-27 22:23:48 +0900

    feat: Rename application runner

--- src/main/java/com/vincent/checklist/TodoApplication.java
+++ src/main/java/com/vincent/checklist/TodoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChecklistApplication {
+public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChecklistApplication.class, args);
+		SpringApplication.run(TodoApplication.class, args);
 	}
 
 }
